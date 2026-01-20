c9eb98b4a17ca6a97cd5e26b9f28a31ff13b67dd
sopheak <sansopheak686@gmail.com>
2026-01-20 21:38:39 +0700

    FIXED

--- db.js
+++ db.js
@@ -2,17 +2,18 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "elearning"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
   if (err) {
-    console.log("DB error", err);
+    console.error("DB connection failed:", err.message);
   } else {
-    console.log("MySQL connected");
+    console.log("MySQL connected successfully!");
   }
 });
 
