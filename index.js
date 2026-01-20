ddf14229e3b1eb59a09ab3aa2c709d5acce4958a
sopheak <sansopheak686@gmail.com>
2026-01-20 20:27:06 +0700

    HI

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const lessonRoutes = require("./routes/lesson.routes");
 const adminRoutes = require("./routes/admin.routes");
 
 const app = express();
+app.get('/test-db', (req, res) => {
+  connection.query('SELECT 1 + 1 AS solution', (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('DB connection failed: ' + err.message);
+    }
+    res.send('DB works! Result: ' + results[0].solution);
+  });
+});
+
 
 app.use(cors());
 app.use(express.json());
