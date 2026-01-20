399c468a5c8aa6c2a1b12bdf4d8955b22d00cec9
sopheak <sansopheak686@gmail.com>
2026-01-20 14:55:21 +0700

    FIX

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth.routes");
+const courseRoutes = require("./routes/course.routes");
+const enrollmentRoutes = require("./routes/enrollment.routes");
+const lessonRoutes = require("./routes/lesson.routes");
+const adminRoutes = require("./routes/admin.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/enrollments", enrollmentRoutes);
+app.use("/api", lessonRoutes);
+app.use("/api/admin", adminRoutes);
+
+const swaggerSetup = require("./swagger");
+swaggerSetup(app);
+
+app.get("/", (req, res) => {
+  res.send("E-Learning API running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
