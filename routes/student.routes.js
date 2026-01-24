45e6438170273b876a30c3b4c7a2d6590ac213b4
sopheak <sansopheak686@gmail.com>
2026-01-24 15:19:14 +0700

    MMM

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { verifyToken } = require("../middlewares/auth.middleware");
-const { dashboardController } = require("../controllers/dashboard.controller");
+const { getDashboard } = require("../controllers/dashboard.controller");
 
 
 // Dashboard
-router.get("/dashboard", verifyToken, dashboardController);
-
+router.get("/dashboard", verifyToken, getDashboard);
 module.exports = router;
