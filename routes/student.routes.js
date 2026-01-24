e4c2ebcc0968c7dfba0241ba919f5dedf4b69d37
sopheak <sansopheak686@gmail.com>
2026-01-24 15:03:37 +0700

    edit student.route

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../middlewares/auth.middleware");
-const dashboardController = require("../controllers/dashboard.controller");
+const { auth } = require("../middlewares/auth.middleware");
+const { dashboardController } = require("../controllers/dashboard.controller");
+
 
 // Dashboard
 router.get("/dashboard", auth, dashboardController.getDashboard);
